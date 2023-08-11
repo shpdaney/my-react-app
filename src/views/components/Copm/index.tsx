f2f6a4d48e842953bdd42e04321e0b40dc2aec5b
Shpala <danila.shpala@yandex.ru>
2023-08-11 18:22:17 +0300

    init

---
+++ src/views/components/Copm/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './index.module.scss'
+
+export const Comp: React.FC = (/*{Props}*/) => {
+    return (
+        <section className={styles.comp}>
+            <h2 className={styles.compTitle}>COMP</h2>
+        </section>
+    )
+}
